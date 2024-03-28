a559d1d0e94298b6ba47000a574fd699c60d917b
DevHeart1 <obasipeace412@yahoo.com>
2024-03-28 15:50:22 +0100

    Add: added some templates

--- users/views.py
+++ users/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts=[
+    {
+        'author': 'Obasi Peace',
+        'title': 'The Inhive Project',
+        'content': 'Learning the backend part',
+        'date_posted': 'March 28,2024'
+
+    },
+    {
+        'author': 'Obasi Oluchi',
+        'title': 'The Inhive Project cont.',
+        'content': 'Learning the backend part 2',
+        'date_posted': 'March 29,2024'
+    }
+]
+
+
+
 def home(request):
-    return render(request, 'users/home.html', )
+    context = {
+        'posts': posts
+    }
+    return render(request, 'users/home.html', context)
 
 def signup(request):
-    return render(request, 'users/signup.html', )
+    return render(request, 'users/signup.html', {'title': 'Signup'} )
