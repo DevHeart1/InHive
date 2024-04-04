0be0e133994761ee8e29143b56f02f96a70323dc
DevHeart1 <obasipeace412@yahoo.com>
2024-04-04 11:23:54 +0000

    added a new app for the user registration

--- users/views.py
+++ users/views.py
@@ -1,29 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts=[
-    {
-        'author': 'Obasi Peace',
-        'title': 'The Inhive Project',
-        'content': 'Learning the backend part',
-        'date_posted': 'March 28,2024'
-
-    },
-    {
-        'author': 'Obasi Oluchi',
-        'title': 'The Inhive Project cont.',
-        'content': 'Learning the backend part 2',
-        'date_posted': 'March 29,2024'
-    }
-]
-
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'users/home.html', context)
 
-def signup(request):
-    return render(request, 'users/signup.html', {'title': 'Signup'} )
+def about(request):
+    return render(request, 'users/about.html', {'title': 'About'} )
